ba3bb1db87e20de08651caa5e34fcca81600b7e4
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-03-05 10:35:21 +0100

    Save progress: 2026-03-05

--- Sources/Tests Core/exports.swift
+++ Sources/Tests Core/exports.swift
@@ -1,4 +1,5 @@
 @_exported public import Test_Primitives
 @_exported public import Set_Primitives
+@_exported public import Ownership_Primitives
 @_exported public import Witnesses
 @_exported public import Tree_Keyed_Primitives
